37bbbc4ef1e06bd634e88852439a33446cd9092f
Alexandr Naum <mfq07k9cz@gmail.com>
2022-06-27 22:23:16 +0300

    marvel project

---
+++ src/services/MarvelService.js
@@ -0,0 +1,44 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=72e5fd280398cdce6c2d8429c281338f';
+    _baseOffset = 210;
+
+    getResourse = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Cloud not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+   }
+
+   getAllCharacters = async (offset = this._baseOffset) => {
+
+        const res = await this.getResourse(`${this._apiBase}/characters?limit=9&offset=${offset}&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+
+   }
+
+   getCharacter = async (id) => {
+
+        const res = await this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+        
+    }
+    
+    _transformCharacter = (char) => {
+    
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description  ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+            comics: char.comics.items
+        }
+    }
+}
+
+export default MarvelService;
